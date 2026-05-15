e8f4e28deaef884970291e9f656dfdaf4f85026d
rhit-silkaijr <silkaijr@rose-hulman.edu>
2026-05-15 10:48:55 -0400

    Starting lin reg work

--- Model1D.m
+++ Model1D.m
@@ -15,8 +15,7 @@ end
 
 %1D spin config
 
-sizeSigma = 100;
-%sigma = [1 1 -1 1 1 1 1 -1];
+sizeSigma = 200;
 sigma = ones(sizeSigma,1);
 for i=1:sizeSigma
     randVar = rand;
@@ -24,18 +23,15 @@ for i=1:sizeSigma
         sigma(i) = -1;
     end
 end
-sigmaStart = sigma;
 
-numIters = 500;
-reversalProb = .6;
-%T = 1;
-T = linspace(1,4,20);
+numIters = 1000;
+reversalProb = .25;
+T = linspace(1,4,200);
 A = adjMatrix;
 currEnergy = zeros(numIters+1,length(T));
 currMag = zeros(numIters+1,length(T));
 iter = linspace(0,numIters,numIters+1);
 for t=1:length(T)
-    sigma = sigmaStart;
     currEnergy(1,t) = ESigma(sigma, A);
     currMag(1,t) = sum(sigma);
     
@@ -81,8 +77,33 @@ for i=1:length(T)
     MTN(i) = mean(MT)/numIters;
     CHITN(i) = (mean(MT.^2)-mean(MT)^2)/(i*numIters);
 end
+ETN = -ETN(2:length(ETN));
 
-%scatter(T, ETN);
+scatter(T(2:length(T)), ETN);
+ylabel('Sample Energy per N');
+xlabel('Temperature (T)');
 %scatter(T, CTN);
+%ylabel('Sample Heat Capacity per N');
+%xlabel('Temperature (T)');
 %scatter(T, MTN);
+%ylabel('Sample Abs. Mag. per N');
+%xlabel('Temperature (T)');
 %scatter(T, CHITN);
+%ylabel('Sample Susceptibility per N');
+%xlabel('Temperature (T)');
+
+
+% Now do it for different sizes of sigma
+% and use the results for a lin reg
+nspace = linspace(4,20,17);
+for n=nspace
+    sizeSigma = n;
+    sigma = ones(sizeSigma,1);
+    for i=1:sizeSigma
+        randVar = rand;
+        if randVar > 0.6
+            sigma(i) = -1;
+        end
+    end
+    sigmaStart = sigma;
+end
